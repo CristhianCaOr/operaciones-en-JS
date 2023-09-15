a945991f57b13346d77cee343048157fe3a6f7fa
Cristhian <cristiancanizarezortiz@gmail.com>
2023-09-15 18:29:37 -0500

    hice un cambio en las opciones del JS

--- operaciones.js
+++ operaciones.js
@@ -4,25 +4,27 @@ let pregunta = ""
 
 numero1 = prompt("dime un numero")
 numero2 = prompt("dime otro numero")
-pregunta = prompt("¿Que quieres que haga? \n Sumar \n Restar \n multiplicar \n dividir")
+pregunta = prompt("¿Que quieres que haga? \n 1: Sumar \n 2: Restar \n 3: multiplicar \n 4: dividir")
 
-if(pregunta == "sumar"){
+if(pregunta == "1"){
 
 alert("la respuesta es " + (parseFloat(numero1) + parseFloat(numero2)))
 
-} else if (pregunta == "restar"){
+} else if (pregunta == "2"){
 
     alert("la respuesta es " + (numero1 - numero2))
 
 }
 
-else if (pregunta == "multiplicar"){
+else if (pregunta == "3"){
 
     alert("la respuesta es " + (numero1 * numero2))
 
-} else if (pregunta == "dividir"){
+} else if (pregunta == "4"){
 
     alert("la respuesta es " + (numero1 / numero2))
 
 }
+
+
 
